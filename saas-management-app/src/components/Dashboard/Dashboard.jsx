69367a965d60f57435763a8b9c278238561c0ff5
AbdullahTalhaAkpur <akpurrabdullah@gmail.com>
2024-05-11 15:11:43 +0300

    we fix the version differnt

--- saas-management-app/src/components/Dashboard/Dashboard.jsx
+++ saas-management-app/src/components/Dashboard/Dashboard.jsx
@@ -7,12 +7,12 @@ import Product from  '../Product/ProductTracking'
 import Graphs from '../Graphs/Graphs'
 import Settings from '../Settings/Settings'
 import Exit from '../Exit/Exit'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
  
 const Dashboard = () => {
   return (
    <BrowserRouter>
-    <Switch>
+    <Routes>
       <Route path='/Team' exact Component={Team}/>
       <Route path='/Facilities' exact Component={Facilities}/>
       <Route path='/Missions' exact Component={Missions}/>
@@ -20,7 +20,7 @@ const Dashboard = () => {
       <Route path='/Graphs' exact Component={Graphs}/>
       <Route path='/Settings' exact Component={Settings}/>
       <Route path='/Exit' exact Component={Exit}/>
-    </Switch>   
+    </Routes>   
    </BrowserRouter>
   )
 }
