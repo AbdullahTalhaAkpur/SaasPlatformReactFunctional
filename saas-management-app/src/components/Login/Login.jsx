8ea54c2a2404a47d7f6fb0a7b16beb3c0416fcb8
AbdullahTalhaAkpur <akpurrabdullah@gmail.com>
2024-05-17 00:42:28 +0300

    we can write login page

---
+++ saas-management-app/src/components/Login/Login.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import './login.css'
+import logo from '../assets/agriverts-555.png'
+
+
+const Login = ({ onSwitchForm }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
