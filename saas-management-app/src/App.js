c74bf743c76b512b19d6c02a779b2ca6b0a22328
AbdullahTalhaAkpur <akpurrabdullah@gmail.com>
2024-05-17 00:56:58 +0300

    we finishde login and register page

--- saas-management-app/src/App.js
+++ saas-management-app/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
+  const [formType, setFormType] = React.useState('login');
+
+  const handleSwitchForm = (type) => {
+    setFormType(type);
+  };
+
   return (
-    <div className="App">
-      <Login /> 
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>{formType === 'login' ? <Login onSwitchForm={handleSwitchForm} /> : <Register onSwitchForm={handleSwitchForm} />}</div>} />
+        <Route path='/Dashboard/*' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ saas-management-app/src/components/Register/Register.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import './register.css'
+import logo from '../assets/agriverts-555.png';
+import { useNavigate } from 'react-router-dom'
+import { register } from '../auth/auth';
+
+const Register = ({ onSwitchForm }) => {
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const navigate = useNavigate();
+ 
+  const handleSubmit = (e) => {
+        e.preventDefault();
+        try {
+          register(email, pass, name, username)
+          navigate('/Dashboard');        
+        } catch (error) {
+          alert(error.message)
+        }
+  }
+  
+  return (
+          <div className="register-container">
+          <div className="logo-container">
+            <img src={logo} alt="Agriverts logo" />
+          </div>
+       <div className="register-card-container">
+       <div className="register-card">
+          <h2 className='register'>Register</h2>
+       <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Full Name</label>
+          <input value={name} onChange={(e) => setName(e.target.value)} type='text' name='name' id='name' ></input>
+          <label htmlFor="username">Username</label>
+          <input value={username} onChange={(e) => setUsername(e.target.value)} type='text' name='username' id='username' ></input>
+          <label htmlFor="email">Email</label>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email" />
+          <label htmlFor="password">Password</label>
+          <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password" />
+          <button type="submit">Register</button>
+          <p>Already have an account ? <a className='loginlink' href='#login' onClick={() => onSwitchForm('login')}>Login</a></p>
+       </form>
+         </div>
+        </div>
+       </div>
+  )
+}
+
+
+export default Register
