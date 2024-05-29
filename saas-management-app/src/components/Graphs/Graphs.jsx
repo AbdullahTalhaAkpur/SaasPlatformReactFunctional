0c3a374f6022b0e45e781c8d812f1cfcf081179c
AbdullahTalhaAkpur <akpurrabdullah@gmail.com>
2024-05-29 22:34:07 +0300

    we are trying graphs settings

--- saas-management-app/src/components/Graphs/Graphs.jsx
+++ saas-management-app/src/components/Graphs/Graphs.jsx
@@ -1,38 +1,60 @@
-import React from 'react'
-import './graphs.css'
-import { BarChart, LineChart } from '@mui/x-charts';
+import React from 'react';
+import { LineChart, Line, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import './graphs.css';
 
+const lineChartData = [
+  { time: '21:14:18', value: 6 },
+  { time: '21:14:26', value: 5.5 },
+  { time: '21:14:31', value: 7 },
+  { time: '21:14:37', value: 8.5 },
+  { time: '21:14:42', value: 9 },
+  { time: '21:14:49', value: 10 },
+  { time: '21:14:53', value: 9.5 },
+  { time: '21:15:02', value: 11 },
+];
+
+const barChartData = [
+  { time: '21:14:18', value: 6 },
+  { time: '21:14:26', value: 5.5 },
+  { time: '21:14:31', value: 7 },
+  { time: '21:14:37', value: 8.5 },
+  { time: '21:14:42', value: 9 },
+  { time: '21:14:49', value: 10 },
+  { time: '21:14:53', value: 9.5 },
+  { time: '21:15:02', value: 11 },
+];
 
 const Graphs = () => {
   return (
-    <div>
-    <LineChart
-      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-      series={[
-        {
-          data: [2, 5.5, 2, 8.5, 1.5, 5],
-        },
-      ]}
-      width={500}
-      height={300}
-    />
-    <BarChart 
-       series={[
-        { data: [35, 44, 24, 34]},
-        { data: [35, 44, 24, 34]},
-        { data: [15, 25, 30, 50]},
-        { data: [60, 50, 15, 25]},
-        { data: [21, 12, 36, 56]},
-        { data: [3, 6, 9, 12]},
-        { data: [4, 8, 12, 16]},
-        { data: [52, 54, 33, 17]},
-       ]}
-       height={290}
-       xAxis={[{ data: ['Q1','Q2','Q3','Q4'], scaleType: 'band' }]}
-       margin= {{ top: 10, bottom: 30, left: 40, right: 10}}
-    />
+    <div className="graphs-container">
+      <div className="chart">
+        <h3>MARUL GÖBEKLİ</h3>
+        <ResponsiveContainer width="100%" height={300}>
+          <LineChart data={lineChartData} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="time" label={{ value: 'Zaman', position: 'insideBottomRight', offset: -5 }} />
+            <YAxis label={{ value: 'Büyüklük', angle: -90, position: 'insideLeft' }} />
+            <Tooltip />
+            <Legend verticalAlign="top" height={36} />
+            <Line type="monotone" dataKey="value" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+      <div className="chart">
+        <h3>MARUL GÖBEKLİ</h3>
+        <ResponsiveContainer width="100%" height={300}>
+          <BarChart data={barChartData} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="time" label={{ value: 'Zaman', position: 'insideBottomRight', offset: -5 }} />
+            <YAxis label={{ value: 'Büyüklük', angle: -90, position: 'insideLeft' }} />
+            <Tooltip />
+            <Legend verticalAlign="top" height={36} />
+            <Bar dataKey="value" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Graphs
+export default Graphs;
--- saas-management-app/src/components/Sidebar/Sidebar.jsx
+++ saas-management-app/src/components/Sidebar/Sidebar.jsx
@@ -15,10 +15,7 @@ import { Link} from 'react-router-dom';
 import Exit from '../Exit/Exit';
 
 
-
 const Sidebar = () => {
-
-  
 
   return (
     <Drawer
