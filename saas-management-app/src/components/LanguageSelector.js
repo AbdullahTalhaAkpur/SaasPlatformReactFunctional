9d5c265bc3bc5ffd5517def985e86a4947b12317
AbdullahTalhaAkpur <akpurrabdullah@gmail.com>
2024-06-21 16:03:30 +0300

    we are creating languageselector.js

---
+++ saas-management-app/src/components/LanguageSelector.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+const LanguageSelector = () => {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <div>
+      <button onClick={() => changeLanguage('en')}>English</button>
+      <button onClick={() => changeLanguage('tr')}>Türkçe</button>
+    </div>
+  );
+};
+
+export default LanguageSelector;
