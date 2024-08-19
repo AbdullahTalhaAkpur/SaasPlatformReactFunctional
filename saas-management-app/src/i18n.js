8feb6011d930c22b424957ec3b646971a9e4e059
AbdullahTalhaAkpur <akpurrabdullah@gmail.com>
2024-08-19 16:05:59 +0300

    WE STİLL TRYING

--- saas-management-app/src/i18n.js
+++ saas-management-app/src/i18n.js
@@ -2,25 +2,18 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 
-const resources = {
-  tr: {
-    translation: {
-      giriş: 'Giriş'
-    }
-  },
-  en: {
-    translation: {
-      login: 'Login'
-    }
-  }
-}
-
 i18n
   .use(initReactI18next)
   .init({
-    resources,
-    lng: "tr", // Default Language
-    supportedLngs: ['en', 'tr'],
+    resources: {
+      en: {
+        translation: require('./locales/enTranslation.json')
+      },
+      tr: {
+        translation: require('./locales/trTranslation.json')
+      }
+    },
+    lng: localStorage.getItem('language') || 'tr', // Default language from localStorage or 'tr'
     fallbackLng: 'tr',
     debug: true,
     interpolation: {
