ed9e2828eb8dfe099c6c92fcdb7089f72153db9d
AbdullahTalhaAkpur <akpurrabdullah@gmail.com>
2024-08-21 17:31:36 +0300

    we had moved language button to headers

--- saas-management-app/src/components/Header/Header.jsx
+++ saas-management-app/src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 // src/components/Header.jsx
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Box, Badge } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton, Box, Badge, Select, MenuItem } from '@mui/material';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MenuIcon from '@mui/icons-material/Menu';
+import { useTranslation } from 'react-i18next';
 
 const Header = () => {
+  const {i18n} = useTranslation();
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+    localStorage.setItem('language', lang)
+  }
   return (
     <AppBar position="fixed" sx={{backgroundColor: '#bad28f', zIndex: theme => theme.zIndex.drawer + 1 }}>
       <Toolbar>
@@ -21,6 +27,15 @@ const Header = () => {
           Dashboard
         </Typography>
         <Box sx={{ flexGrow: 1 }} />
+        <Select
+          value={i18n.language}
+          onChange={(e) => changeLanguage(e.target.value)}
+          sx={{ color: 'white', borderColor: 'white', mr: 2 }}
+          variant="outlined"
+        >
+          <MenuItem value="en">English</MenuItem>
+          <MenuItem value="tr">Turkish</MenuItem>
+        </Select>
         <IconButton color="inherit">
           <Badge badgeContent={4} color="secondary">
             <NotificationsIcon />
