56e09b14becaf586e983f3fb87cb138185116461
AbdullahTalhaAkpur <akpurrabdullah@gmail.com>
2024-08-22 00:22:54 +0300

    WE SOLVE HEADER.JSX

--- saas-management-app/src/components/Header/Header.jsx
+++ saas-management-app/src/components/Header/Header.jsx
@@ -1,17 +1,12 @@
 // src/components/Header.jsx
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Box, Badge, Select, MenuItem } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton, Box, Badge } from '@mui/material';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MenuIcon from '@mui/icons-material/Menu';
-import { useTranslation } from 'react-i18next';
+import LanguageSelector from '../LanguageSelector/LanguageSelector';
 
 const Header = () => {
-  const {i18n} = useTranslation();
-  const changeLanguage = (lang) => {
-    i18n.changeLanguage(lang);
-    localStorage.setItem('language', lang)
-  }
   return (
     <AppBar position="fixed" sx={{backgroundColor: '#bad28f', zIndex: theme => theme.zIndex.drawer + 1 }}>
       <Toolbar>
@@ -27,15 +22,7 @@ const Header = () => {
           Dashboard
         </Typography>
         <Box sx={{ flexGrow: 1 }} />
-        <Select
-          value={i18n.language}
-          onChange={(e) => changeLanguage(e.target.value)}
-          sx={{ color: 'white', borderColor: 'white', mr: 2 }}
-          variant="outlined"
-        >
-          <MenuItem value="en">English</MenuItem>
-          <MenuItem value="tr">Turkish</MenuItem>
-        </Select>
+         <LanguageSelector />
         <IconButton color="inherit">
           <Badge badgeContent={4} color="secondary">
             <NotificationsIcon />
